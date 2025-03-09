c3eb1be06e1876105ede3930f2ee367f2e11f79f
arun-kumar-bidila <arunkumarbidila@gmail.com>
2025-03-09 18:32:05 +0530

    added get user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,8 +47,24 @@ const updateUserDetails = async (req, res) => {
 
 
 
+
+const getUserDetails = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
+
 module.exports = {
     createUser,
     updateUserDetails,
- 
+    getUserDetails, // Exporting the new function
 };
+
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { createUser, updateUserDetails} = require("../controllers/userController");
+const { createUser, updateUserDetails,getUserDetails} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/", createUser); // Step 1: Create user (name & email)
 router.put("/:id", updateUserDetails); // Step 2: Update skills, educationLevel, and experience
-
+router.get("/:id", getUserDetails);
 
 module.exports = router;
 
